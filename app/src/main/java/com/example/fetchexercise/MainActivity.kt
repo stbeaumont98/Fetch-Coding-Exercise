79c6da07df4a4fe1c4dc9411a41d515d9b4f5bca
Spencer Beaumont <spence.beaumont@gmail.com>
2025-05-02 12:41:28 -0400

    Initialize MainViewModel and display the composable MainScreen.

--- app/src/main/java/com/example/fetchexercise/MainActivity.kt
+++ app/src/main/java/com/example/fetchexercise/MainActivity.kt
@@ -4,44 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import com.example.fetchexercise.ui.main.MainScreen
+import com.example.fetchexercise.ui.main.MainViewModel
 import com.example.fetchexercise.ui.theme.FetchExerciseTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val mainViewModel: MainViewModel = MainViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FetchExerciseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+                    mainViewModel.loadData()
+                    val state = mainViewModel.state.collectAsState()
+                    MainScreen(state.value)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FetchExerciseTheme {
-        Greeting("Android")
     }
 }
